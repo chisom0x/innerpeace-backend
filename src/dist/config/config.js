9c758600d7650c721348d887d07a74a8b0622100
chisomOx <onyenankiekelvin@gmail.com>
2024-11-06 17:35:28 +0100

    modified:   src/dist/config/config.js 	modified:   src/dist/config/config.js.map

--- src/dist/config/config.js
+++ src/dist/config/config.js
@@ -6,18 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-// Log environment variables to verify they are loaded correctly
-console.log('NODE_ENV:', process.env.NODE_ENV);
-console.log('DB_DEV_USERNAME:', process.env.DB_DEV_USERNAME);
-console.log('DB_DEV_PASSWORD:', process.env.DB_DEV_PASSWORD);
-console.log('DB_DEV_DATABASE:', process.env.DB_DEV_DATABASE);
-console.log('DB_DEV_HOST:', process.env.DB_DEV_HOST);
-console.log('DB_DEV_PORT:', process.env.DB_DEV_PORT);
-console.log('DB_PROD_USERNAME:', process.env.DB_PROD_USERNAME);
-console.log('DB_PROD_PASSWORD:', process.env.DB_PROD_PASSWORD);
-console.log('DB_PROD_DATABASE:', process.env.DB_PROD_DATABASE);
-console.log('DB_PROD_HOST:', process.env.DB_PROD_HOST);
-console.log('DB_PROD_PORT:', process.env.DB_PROD_PORT);
 // Determine the environment (default to 'development' if not specified)
 const env = process.env.NODE_ENV || 'development';
 // Set up database credentials based on the current environment
@@ -37,6 +25,5 @@ const sequelize = new sequelize_1.Sequelize(dbConfig.database, dbConfig.username
     dialect: dbConfig.dialect,
     logging: dbConfig.logging,
 });
-console.log('Database Config:', dbConfig);
 exports.default = sequelize;
 //# sourceMappingURL=config.js.map
